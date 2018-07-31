1b5b310661218b78ba9fcb2a739c0ccc02c82a0c
Jordan Opdahl <opdah027@umn.edu>
2018-07-31 18:19:00 -0500

    Added Clear Score function

--- ohHeck/player.py
+++ ohHeck/player.py
@@ -1,2 +1,24 @@
 class Player:
     
+    def __init__(self,name):
+        self.name = name
+        self.score = 0
+        self.__hand = []
+
+    def addCard(self,card):
+        self.__hand.append(card)
+    
+    def sortHand(self):
+        self.__hand = sorted(self.__hand)
+    
+    def returnHand(self):
+        return self.__hand
+
+    def addScore(self,points):
+        self.score = self.score + 10 + points
+    
+    def viewScore(self):
+        return self.score
+    
+    def clearHand(self):
+        self.__hand = []
